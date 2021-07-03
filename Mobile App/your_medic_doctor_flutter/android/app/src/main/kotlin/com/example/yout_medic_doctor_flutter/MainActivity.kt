48d3e242d19e57da10972ef1eb8d011fab2476c4
dhruv-mehta99 <dhruvmehta212@gmail.com>
2021-07-03 20:13:29 +0530

    trying to make the folder structure properly

--- Mobile App/your_medic_doctor_flutter/android/app/src/main/kotlin/com/example/yout_medic_doctor_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.yout_medic_doctor_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Mobile App/your_medic_doctor_flutter/lib/profileClasses/Vitals.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Vitals {
-  int reading;
-  String time;
-  Vitals({required this.reading, required this.time});
-}
